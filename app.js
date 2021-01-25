ae1368547154f96109136074a3a422f2ccab1a3b
alex <alex_kobilianskiy@bigmir.net>
2021-01-25 22:20:19 +0200

    connect to mongodb

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-// const express = require('express');
-import express from 'express';
-// const config = require('config');
-import config from 'config';
+const express = require('express');
+// import express from 'express';
+const config = require('config');
+// import config from 'config';
+// import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 const app = express();
 
 const PORT = config.get('port') || 5000;
 
-app.listen(PORT, () => console.log(`App has been started on port ${PORT} !`));
+async function start() {
+    try {
+       await mongoose.connect(config.get('mongoUri'), {
+           useNewUrlParser: true,
+           useUnifiedTopology: true,
+           useCreateIndex: true
+        })
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT} !`));
+    } catch (e) {
+        console.log('Server Error', e.message);
+        process.exit(1);
+    }
+}
+
+start();
+
