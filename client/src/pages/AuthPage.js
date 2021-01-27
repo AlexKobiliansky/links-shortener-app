a6254f0af2da93823890239e72a3a19a450d41dc
alex <alex_kobilianskiy@bigmir.net>
2021-01-27 09:50:36 +0200

    handle errors

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,14 +1,20 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {useHttp} from "../hooks/http.hook";
+import {useMessage} from "../hooks/message.hook";
 
 export const AuthPage = () => {
-
-    const {loading, error, request} = useHttp();
+    const message = useMessage();
+    const {loading, error, request, clearError} = useHttp();
 
     const [form, setForm] = useState({
         email: '',
         password: ''
-    })
+    });
+
+    useEffect(() => {
+        message(error);
+        clearError();
+    }, [error, message, clearError]);
 
     const changeHandler = event => {
         setForm({...form, [event.target.name]: event.target.value});
@@ -17,7 +23,14 @@ export const AuthPage = () => {
     const registerHandler = async () => {
         try {
             const data = await request('/api/auth/register', 'POST', {...form})
-            console.log(data)
+            message(data.message);
+        } catch (e) {}
+    }
+
+    const loginHandler = async () => {
+        try {
+            const data = await request('/api/auth/login', 'POST', {...form})
+            message(data.message);
         } catch (e) {}
     }
 
@@ -63,6 +76,7 @@ export const AuthPage = () => {
                             className="btn yellow darken-4"
                             style={{marginRight: 10}}
                             disabled={loading}
+                            onClick={loginHandler}
                         >
                             Войти
                         </button>
